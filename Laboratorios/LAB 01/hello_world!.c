7947a0797737287585230c0a0d51fabdc3136707
Felipe <fssanchez@uc.cl>
2021-04-06 20:10:23 -0400

    Se suben archivos faltantes

---
+++ Laboratorios/LAB 01/hello_world!.c
@@ -0,0 +1,17 @@
+/* 
+Name: Hello_world!.c
+Author: Felipe Sanchez Varas
+Version: 1.0
+Date: 14-08-2020
+Comments:
+	- Archivo de ejemplo para el curso IEE2463 SEP. 
+*/
+
+#include <stdio.h>
+
+int main() {
+   /* Primer programa en C */
+   printf("Hello, World! \n");
+   
+   return 0;
+}
