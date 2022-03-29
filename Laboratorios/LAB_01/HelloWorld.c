1ab3392e7ec55be233f30e92a0797ed650c91640
NicolasVillarroel8 <81389008+NicolasVillarroel8@users.noreply.github.com>
2022-03-29 19:21:14 -0300

    Create HelloWorld.c

---
+++ Laboratorios/LAB_01/HelloWorld.c
@@ -0,0 +1,15 @@
+/*
+ *
+ *  Fecha Entrega: 29-03-2022
+ *      Author: Nicolas Villarroel
+ *      Código: Hello, World!
+ *
+ */
+
+#include <stdio.h>
+
+void main (){
+  
+  printf("Hello, World!");
+
+}
